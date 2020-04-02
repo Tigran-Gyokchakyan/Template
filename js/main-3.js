8967421a9169d7b468b0155a89f4a8ba72c750bb
Tigran-Gyokchakyan <tigrangyokchakyan93@gmail.com>
2020-04-02 17:53:19 +0400

    Home-3

--- js/main-3.js
+++ js/main-3.js
@@ -18,3 +18,33 @@ TABLE OF CONTENT
 /*-----------------------------------------
 1. Header
 -------------------------------------------*/
+/*Nav Bar*/
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+	burger.addEventListener('click', () => {
+		/*Toggle nav*/
+		nav.classList.toggle('nav-active');
+		/*Animate Links*/
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
+			}
+		});
+		/* Burger Animation*/
+		burger.classList.toggle('toggle')
+	});
+}
+navSlide();
+/*NavBar Stick*/
+const navHeight = $("nav").height();
+$(window).scroll(function () {
+	if ($(this).scrollTop() > navHeight) {
+		$('.navMenu').addClass('fixedColor');
+	} else {
+		$('.navMenu').removeClass('fixedColor');
+	}
+})
