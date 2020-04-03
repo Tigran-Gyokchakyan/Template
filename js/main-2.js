97f65915991d8d62f565b083816173f3b0a15107
Tigran-Gyokchakyan <tigrangyokchakyan93@gmail.com>
2020-04-03 17:24:00 +0400

    Home-3 Finished

--- js/main-2.js
+++ js/main-2.js
@@ -23,60 +23,60 @@ TABLE OF CONTENT
 -------------------------------------------*/
 /*Nav Bar*/
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
-	burger.addEventListener('click', () => {
-		/*Toggle nav*/
-		nav.classList.toggle('nav-active');
-		/*Animate Links*/
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
-			}
-		});
-		/* Burger Animation*/
-		burger.classList.toggle('toggle')
-	});
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  burger.addEventListener('click', () => {
+    /*Toggle nav*/
+    nav.classList.toggle('nav-active');
+    /*Animate Links*/
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
+      }
+    });
+    /* Burger Animation*/
+    burger.classList.toggle('toggle')
+  });
 }
 navSlide();
 /*NavBar Stick*/
 const navHeight = $("nav").height();
-$(window).scroll(function () {
-	if ($(this).scrollTop() > navHeight) {
-		$('.navMenu').addClass('fixed');
-	} else {
-		$('.navMenu').removeClass('fixed');
-	}
+$(window).scroll(function() {
+  if ($(this).scrollTop() > navHeight) {
+    $('.navMenu').addClass('fixed');
+  } else {
+    $('.navMenu').removeClass('fixed');
+  }
 })
 /*-----------------------------------------
 2. Brends
 -------------------------------------------*/
 /*owl-carousel brends*/
-$(document).ready(function () {
-	$("#owl-brends").owlCarousel({
-		loop: true,
-		items: 6,
-		nav: false,
-		dots: false,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: false,
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 3,
-			},
-			600: {
-				items: 3,
-			},
-			1000: {
-				items: 6,
-			}
-		}
-	})
+$(document).ready(function() {
+  $("#owl-brends").owlCarousel({
+    loop: true,
+    items: 6,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      }
+    }
+  })
 });
 /*-----------------------------------------
 3. Player
@@ -85,7 +85,7 @@ var templateElement = document.getElementById('video');
 var template = templateElement.innerHTML;
 var posterElement = document.getElementById('video-poster');
 var videoElement = document.getElementById('video-element');
-posterElement.addEventListener('click', function () {
-	videoElement.removeChild(posterElement);
-	videoElement.innerHTML += template;
+posterElement.addEventListener('click', function() {
+  videoElement.removeChild(posterElement);
+  videoElement.innerHTML += template;
 });
--- js/main.js
+++ js/main.js
@@ -22,81 +22,81 @@ TABLE OF CONTENT
 -------------------------------------------*/
 /*Nav Bar*/
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
-	burger.addEventListener('click', () => {
-		/*Toggle nav*/
-		nav.classList.toggle('nav-active');
-		/*Animate Links*/
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
-			}
-		});
-		/* Burger Animation*/
-		burger.classList.toggle('toggle')
-	});
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  burger.addEventListener('click', () => {
+    /*Toggle nav*/
+    nav.classList.toggle('nav-active');
+    /*Animate Links*/
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
+      }
+    });
+    /* Burger Animation*/
+    burger.classList.toggle('toggle')
+  });
 }
 navSlide();
 /*NavBar Stick*/
 const navHeight = $("nav").height();
-$(window).scroll(function () {
-	if ($(this).scrollTop() > navHeight) {
-		$('.navMenu').addClass('fixedColor');
-	} else {
-		$('.navMenu').removeClass('fixedColor');
-	}
+$(window).scroll(function() {
+  if ($(this).scrollTop() > navHeight) {
+    $('.navMenu').addClass('fixedColor');
+  } else {
+    $('.navMenu').removeClass('fixedColor');
+  }
 })
 /*Anchor Smooth scrolling*/
-$(document).on('click', 'a[href^="#about"]', function (event) {
-	event.preventDefault();
-	$('html, body').animate({
-		scrollTop: $($.attr(this, 'href')).offset().top - 80
-	}, 1000);
+$(document).on('click', 'a[href^="#about"]', function(event) {
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top - 80
+  }, 1000);
 });
 /*-----------------------------------------
 2. Reviews
 -------------------------------------------*/
 /*owl-carousel reviresa*/
-$(document).ready(function () {
-	$("#owl-reviews").owlCarousel({
-		loop: true,
-		items: 1,
-		margin: 10,
-		nav: false,
-		dots: true,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: false
-	})
+$(document).ready(function() {
+  $("#owl-reviews").owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: false
+  })
 });
 /*-----------------------------------------
 3. Brends
 -------------------------------------------*/
 /*owl-carousel brends*/
-$(document).ready(function () {
-	$("#owl-brends").owlCarousel({
-		loop: true,
-		items: 6,
-		nav: false,
-		dots: false,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: false,
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 3,
-			},
-			600: {
-				items: 3,
-			},
-			1000: {
-				items: 6,
-			}
-		}
-	})
+$(document).ready(function() {
+  $("#owl-brends").owlCarousel({
+    loop: true,
+    items: 6,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      }
+    }
+  })
 });
