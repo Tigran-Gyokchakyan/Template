ed35546625b981886a0468a2a01a77abe6206a34
Tigran-Gyokchakyan <tigrangyokchakyan93@gmail.com>
2020-04-04 22:58:35 +0400

    home-4 Editing

--- js/main-4.js
+++ js/main-4.js
@@ -21,37 +21,36 @@ TABLE OF CONTENT
 /*-----------------------------------------
 1. Header
 -------------------------------------------*/
-// /*Nav Bar*/
-// const navSlide = () => {
-//   const burger = document.querySelector('.burger');
-//   const nav = document.querySelector('.nav-links');
-//   const navLinks = document.querySelectorAll('.nav-links li');
-//   burger.addEventListener('click', () => {
-//     /*Toggle nav*/
-//     nav.classList.toggle('nav-active');
-//     /*Animate Links*/
-//     navLinks.forEach((link, index) => {
-//       if (link.style.animation) {
-//         link.style.animation = '';
-//       } else {
-//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
-//       }
-//     });
-//     /* Burger Animation*/
-//     burger.classList.toggle('toggle')
-//   });
-// }
-// navSlide();
-// /*NavBar Stick*/
-// const navHeight = $("nav").height();
-// $(window).scroll(function() {
-//   if ($(this).scrollTop() > navHeight) {
-//     $('.navMenu').addClass('fixedColor');
-//   } else {
-//     $('.navMenu').removeClass('fixedColor');
-//   }
-// })
-
+/*Nav Bar*/
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  burger.addEventListener('click', () => {
+    /*Toggle nav*/
+    nav.classList.toggle('nav-active');
+    /*Animate Links*/
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3 + 0.5}s`;
+      }
+    });
+    /* Burger Animation*/
+    burger.classList.toggle('toggle')
+  });
+}
+navSlide();
+/*NavBar Stick*/
+const navHeight = $("nav").height();
+$(window).scroll(function() {
+  if ($(this).scrollTop() > navHeight) {
+    $('.navMenu').addClass('fixed');
+  } else {
+    $('.navMenu').removeClass('fixed');
+  }
+})
 
 /*-----------------------------------------
 2. Brends
