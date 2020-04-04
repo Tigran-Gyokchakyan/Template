bcbd01b2f60e34f0d7ecdbdd25897f09797269f1
Tigran-Gyokchakyan <tigrangyokchakyan93@gmail.com>
2020-04-04 22:30:43 +0400

    home-4 Editing

--- js/main-4.js
+++ js/main-4.js
@@ -10,6 +10,9 @@ TABLE OF CONTENT
 ==============================================
 
 1. Header
+2. Brends
+3. Feedback
+4. Player
 ==============================================
 [END] TABLE OF CONTENT
 ==============================================
@@ -17,7 +20,7 @@ TABLE OF CONTENT
 "use strict";
 /*-----------------------------------------
 1. Header
-// -------------------------------------------*/
+-------------------------------------------*/
 // /*Nav Bar*/
 // const navSlide = () => {
 //   const burger = document.querySelector('.burger');
@@ -48,3 +51,78 @@ TABLE OF CONTENT
 //     $('.navMenu').removeClass('fixedColor');
 //   }
 // })
+
+
+/*-----------------------------------------
+2. Brends
+-------------------------------------------*/
+$(document).ready(function() {
+  $("#owl-brends").owlCarousel({
+    loop: true,
+    items: 6,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      }
+    }
+  })
+});
+
+/*-----------------------------------------
+3. Feedback
+-------------------------------------------*/
+$(document).ready(function() {
+  $("#owl-feedback").owlCarousel({
+    loop: true,
+    items: 1,
+    stagePadding: 130,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        stagePadding: 0,
+      },
+      600: {
+        stagePadding: 50,
+      },
+      1000: {
+        stagePadding: 0,
+      },
+      1200: {
+        stagePadding: 130,
+      }
+    }
+  })
+});
+
+/*-----------------------------------------
+4. Player
+-------------------------------------------*/
+var doom = document.getElementById('player');
+var templateElement = document.getElementById('video');
+var template = templateElement.innerHTML;
+var posterElement = document.getElementById('video-poster');
+var videoElement = document.getElementById('video-element');
+posterElement.addEventListener('click', function() {
+  videoElement.removeChild(posterElement);
+  doom.style.padding = 0;
+  doom.style.height = "400px";
+  videoElement.style.height = "400px";
+  videoElement.innerHTML += template;
+});
